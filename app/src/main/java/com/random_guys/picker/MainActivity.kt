0486346336cada952fac4f74c6e42ec7b822531f
Raymond Tukpe <rtukpe@gmail.com>
2019-08-19 00:40:25 +0100

    chore: replaces sample activity implementation

--- app/src/main/java/com/random_guys/picker/MainActivity.kt
+++ app/src/main/java/com/random_guys/picker/MainActivity.kt
@@ -1,55 +1,28 @@
 package com.random_guys.picker
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.random_guys.pica.*
-import com.random_guys.rv.LoadMoreListener
-import com.random_guys.rv.RecyclerScrollMoreListener
+import com.random_guys.pica.Chu
+import com.random_guys.pica.Contact
+import com.random_guys.pica.Pica
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), ContactSelectedListener, LoadMoreListener {
-
-    private var mContacts = ArrayList<Contact>()
-    private lateinit var contactAdapter: SelectContactAdapter
-    private lateinit var linearLayoutManager: LinearLayoutManager
-    private lateinit var recyclerScrollMoreListener: RecyclerScrollMoreListener
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // init contact picker
-        val pica = Pica(this)
+        button.setOnClickListener {
+            val mMainContacts = ArrayList<Contact>()
 
-        contactAdapter = SelectContactAdapter(this)
-        contactAdapter.mContactSelectedListener = this
-        contactAdapter.loadMoreListener = this
+            // load contacts
+            val pica = Pica(this)
+            pica.load { contacts -> mMainContacts.addAll(contacts) }
 
-        // load contacts into adapter
-        pica.chu { mContacts = it }
-        contactAdapter.add(mContacts.subList(0, 20))
-
-        linearLayoutManager = LinearLayoutManager(this)
-
-        recyclerView.layoutManager = linearLayoutManager
-        recyclerView.adapter = contactAdapter
-
-        recyclerScrollMoreListener = RecyclerScrollMoreListener(linearLayoutManager, contactAdapter)
-        recyclerView.addOnScrollListener(recyclerScrollMoreListener)
-    }
-
-    override fun onLoadMore(page: Int, total: Int) {
-        if (mContacts.size >= total) {
-            contactAdapter.add(mContacts.subList(page * 10, (page * 10) + 10))
-            recyclerView.post { contactAdapter.notifyItemInserted(contactAdapter.itemCount) }
+            // open contacts picker
+            val chooser = Chu(mMainContacts)
+            chooser.show(supportFragmentManager, "")
         }
-    }
-
-    override fun onContactSelected(v: View, position: Int) {
-        Toast.makeText(this, contactAdapter.get(position).toString(), LENGTH_SHORT).show()
     }
 }
