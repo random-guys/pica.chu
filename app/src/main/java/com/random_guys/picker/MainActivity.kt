cde72d06282dcc7963b61788696e07342bbc55be
Raymond Tukpe <rtukpe@gmail.com>
2019-08-24 09:08:11 +0100

    chore: updates sample app

--- app/src/main/java/com/random_guys/picker/MainActivity.kt
+++ app/src/main/java/com/random_guys/picker/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.random_guys.picker
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.random_guys.pica.Chu
 import com.random_guys.pica.Contact
 import com.random_guys.pica.Pica
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Chu.DismissListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +24,18 @@ class MainActivity : AppCompatActivity() {
             pica.load { contacts -> mMainContacts.addAll(contacts) }
 
             // open contacts picker
-            val chooser = Chu(mMainContacts)
+            val chooser = Chu(mMainContacts, this)
             chooser.show(supportFragmentManager, "")
         }
+    }
+
+    override fun onDismissed() {
+
+    }
+
+    private fun hideKeyboard() {
+        val view: View? = currentFocus
+        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
