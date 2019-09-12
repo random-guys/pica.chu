5657280b53e164cf26bfdd260a608ddb5542f4bc
Raymond Tukpe <rtukpe@gmail.com>
2019-09-12 09:16:13 +0100

    chore: removes redundant files

--- pica/src/main/java/com/random_guys/pica/ContactEmail.kt
+++
@@ -1,3 +0,0 @@
-package com.random_guys.pica
-
-data class ContactEmail(val address: String?, val type: String?)
--- pica/src/main/java/com/random_guys/pica/ContactPhone.kt
+++
@@ -1,3 +0,0 @@
-package com.random_guys.pica
-
-data class ContactPhone(val number: String?, val type: String?)
